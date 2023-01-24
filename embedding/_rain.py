8477761de53ba6f40e9b8af879f18fc9e0cce6d0
Karthik Soman <karthiksoman@leo.ucsf.edu>
2023-01-24 15:52:46 -0800

    removed utility script and added that rain class

--- embedding/_rain.py
+++ embedding/_rain.py
@@ -1,6 +1,9 @@
 import os
+import pandas as pd
+import numpy as np
+import networkx as nx
 import joblib
-from ._utility import read_edge_data, create_graph
+from tqdm import tqdm
 
 
 
@@ -11,8 +14,30 @@ class RAIN:
         
     def load_graph(self):
         if not os.path.exists(self.graph_path):
-            create_graph(self.edge_path, self.graph_path)
+            edge_df_ = self.read_edge_data()
+            print('Initiating graph instance ...')
+            graph = nx.Graph()
+            print('Adding edges to graph ...')
+            for index, row in tqdm(edge_df_.iterrows()):
+                graph.add_edge(row['source'], row['target'], edge_type=row["edge_type"])
+            print('Saving graph ...')
+            joblib.dump(graph, self.graph_path)
         self.graph = joblib.load(self.graph_path)
+            
+    def read_edge_data(self):
+        edge_df = pd.read_csv(self.edge_path, sep='\t').drop_duplicates()
+        edge_df = edge_df[["source", "edge_type", "target"]]
+        edge_df.source = edge_df.source.astype(str)
+        edge_df.target = edge_df.target.astype(str)
+        edge_df_ = edge_df.copy()
+        edge_df_.drop("edge_type", axis=1, inplace=True)
+        edge_df_.source = edge_df_.source.astype(str)
+        edge_df_.target = edge_df_.target.astype(str)
+        edge_df_ = pd.DataFrame(np.sort(edge_df_.values, axis=1), index=edge_df_.index, columns=edge_df_.columns).drop_duplicates()
+        edge_df_ = pd.merge(edge_df_, edge_df, on=["source", "target"]).drop_duplicates(subset=["source", "target"])
+        return edge_df_
+    
+
         
     
         
